00b526e7ba2b7935b01505598abb189494cbb6c1
leo <leo@Debian.delma.com.co>
2017-02-08 12:12:11 -0500

    UVA veredict OK

--- Taller primera semana/src/RelationalOperator.java
+++ Taller primera semana/src/RelationalOperator.java
@@ -1,9 +1,42 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigInteger;
+import java.util.Scanner;
+
 
 public class RelationalOperator {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		Scanner scan;
+		File f = new File("C_1.in");
+		if(f.exists()){
+			scan = new Scanner(f);
+		}else{
+			scan = new Scanner(System.in);
+		}
+		
+		String[] input;
+		input=scan.nextLine().split(" ");
+		int casos=Integer.parseInt(input[0]);
+		
+		for(int i=0;i<casos;i++){
+			input=scan.nextLine().split(" ");
+			
+			BigInteger num1=new BigInteger(input[0]);
+			BigInteger num2=new BigInteger(input[1]);
+			int comp=num1.compareTo(num2);
+			if(comp==-1){
+				System.out.println("<");	
+			}else if(comp==0){
+				System.out.println("=");
+			}else{
+				System.out.println(">");
+			}
+			
+		}
 
 	}
+
 
 }
